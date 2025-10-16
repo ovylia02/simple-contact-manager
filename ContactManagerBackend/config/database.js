36a651245886a91c3564da0f51ca4bbddfba9d61
Ovylia <ovylia02@gmail.com>
2025-10-16 15:53:59 +0700

    Setup database and server initialisation. Create Contacts table in database

---
+++ ContactManagerBackend/config/database.js
@@ -0,0 +1,47 @@
+/**
+ * @file database.js
+ * @description Initializes SQLite database connection and creates the Contacts table
+ */
+
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+// Holds the connected database instance
+let db;
+
+/**
+ * Initialize database and create Contacts table
+ */
+async function initDatabase() {
+    console.log("Begin initializing database");
+
+    // The rest of the code will run only when database successfully initialized
+    db = await open({
+        filename: "./ContactManagerBackend/contacts.db",
+        driver: sqlite3.Database
+    });
+
+    // Create Contacts table
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            phone TEXT NOT NULL,
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    `);
+
+    console.log("Successful database initialisation");
+}
+
+/**
+ * Get the database instance
+ * @returns {sqlite3.Database} The database instance
+ */
+function getDatabase() {
+    if (!db) throw new Error("Database not found");
+    return db;
+}
+
+module.exports = { initDatabase, getDatabase };
---
+++ ContactManagerBackend/server.js
@@ -0,0 +1,37 @@
+/**
+ * @file server.js
+ * @description Entry point for Express server
+ */
+
+const express = require("express");
+const { initDatabase } = require("./config/database.js");
+
+// Initialises Express app
+const app = express();
+app.use(express.json());
+
+/**
+ * Start the Express server while initialising database and connects to port 5000
+ */
+async function startServer() {
+    console.log("Starting server");
+
+    // Initialise database
+    await initDatabase();
+
+    // Test if route works properly (server connected)
+    app.get("/", (req, res) => {
+        res.send("Simple Contact Manager");
+    });
+
+    // Connect to port (http://localhost:5000/)
+    const PORT = 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+    console.log("Server has started");
+}
+
+// Start server and check if server failed to start
+startServer().catch((err) => {
+    console.error("Failed to start server: ", err);
+});
