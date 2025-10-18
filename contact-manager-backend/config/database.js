6e2c1dbb6efc637e414d589d965268c47f523361
Ovylia <ovylia02@gmail.com>
2025-10-18 16:55:42 +0700

    Rename ContactManagerBackend to contact-manager-backend because react cannot make a project with capital letters

--- contact-manager-backend/config/database.js
+++ contact-manager-backend/config/database.js
@@ -16,7 +16,7 @@ async function initDatabase() {
 
     // The rest of the code will run only when database successfully initialized
     db = await open({
-        filename: "./ContactManagerBackend/contacts.db",
+        filename: "./contact-manager-backend/contacts.db",
         driver: sqlite3.Database
     });
 
